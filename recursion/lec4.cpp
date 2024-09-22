c85b262026f42c4aa575f9bb5a3c772d3d77663a
harshjangra123 <harsh.jangra.hansi@gmail.com>
2024-09-22 11:04:07 +0530

    recursion new notes

--- recursion/lec4.cpp
+++ recursion/lec4.cpp
@@ -1,1 +1,30 @@
 // print all subsequence -- a sequence which follows an order
+#include<bits/stdc++.h>
+using namespace std;
+void ss(int index,vector<int>& v,vector<int>& curr,vector<vector<int>>& g){
+    if(index >= v.size()){
+        g.push_back(curr);
+        return;
+    }
+    ss(index+1,v,curr,g);
+    curr.push_back(v[index]);
+    
+    ss(index+1,v,curr,g);
+    curr.pop_back();
+
+}
+
+int main(){
+    vector<int> v = {1,3,2};
+    vector<vector<int>> g;
+    vector<int> curr;
+    ss(0,v,curr,g);
+    for(vector<int> i : g){
+        for (int j : i)
+        {
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
